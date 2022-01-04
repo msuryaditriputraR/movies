90c0affec3b6f79f0bf9465cc5702809f1f634c0
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-01-04 16:42:38 +0800

    add webpack

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,13 @@
+class App extends HTMLElement {
+    connectedCallback() {
+        this.render();
+    }
 
+    render() {
+        this.innerHTML = `<h2>Test</h2>`;
+    }
+}
+
+customElements.define('my-app', App);
+const app = document.createElement('my-app');
+document.getElementById('root').appendChild(app);

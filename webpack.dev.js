6058595bf651613290485410dac71e113e7735e8
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-01-07 20:10:38 +0800

    change webpack development config

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,5 +2,9 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
-    mode: 'development'
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist'
+    }
 });

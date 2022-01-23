92fe5d4c5cbe9815230ed1ad6b79cfed48ac17bf
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-01-23 14:58:27 +0800

    add config production  webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,13 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const path = require('path');
+const glob = require('glob');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+
+const PATHS = {
+    src: path.join(__dirname, 'src')
+};
 
 module.exports = merge(common, {
     mode: 'production',
@@ -9,7 +17,36 @@ module.exports = merge(common, {
                 test: /\.js$/,
                 exclude: '/node_modules/',
                 use: 'babel-loader'
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
             }
         ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new PurgecssPlugin({
+            paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true })
+        })
+    ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                styles: {
+                    name: 'styles',
+                    test: /\.(sa|sc|c)ss$/,
+                    chunks: 'all',
+                    enforce: true
+                }
+            }
+        }
     }
 });

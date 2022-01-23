3d101a8c4cdf9782e3299f4af4bf1add5a55c73c
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-01-23 14:58:13 +0800

    add config dev  webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,20 @@ const common = require('./webpack.common');
 
 module.exports = merge(common, {
     mode: 'development',
+    devtool: 'inline-source-map',
     devServer: {
         static: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    { loader: 'css-loader', options: { sourceMap: true } },
+                    { loader: 'postcss-loader', options: { sourceMap: true } },
+                    { loader: 'sass-loader', options: { sourceMap: true } }
+                ]
+            }
+        ]
     }
 });

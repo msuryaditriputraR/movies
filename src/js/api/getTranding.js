3e5ed27a98db0c5cc312aae50919dc7635b5c236
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-01-26 15:35:44 +0800

    Refactoring  get Tranding  API Handler

--- src/js/api/getTranding.js
+++ src/js/api/getTranding.js
@@ -1,14 +1,12 @@
 const axios = require('axios');
 import { base_url, key } from './data';
 
-async function getTranding(url) {
+export default async function getTranding() {
+    const url = base_url + 'trending/movie/week?api_key=' + key;
+
     try {
         return await axios.get(url);
     } catch (error) {
         console.log(error);
     }
 }
-
-const url = base_url + 'trending/movie/week?api_key=' + key;
-
-export default getTranding(url);

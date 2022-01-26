0b066f4d36fe99d86450d88b75f31a8253267d3f
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-01-26 15:35:07 +0800

    Refactoring  get Movies  API Handler

--- src/js/api/getMovies.js
+++ src/js/api/getMovies.js
@@ -1,14 +1,11 @@
 const axios = require('axios');
 import { base_url, key } from './data';
 
-async function getMovies(url) {
+export default async function getMovies() {
+    const url = base_url + 'discover/movie?api_key=' + key;
     try {
         return await axios.get(url);
     } catch (error) {
         console.log(error);
     }
 }
-
-const url = base_url + 'discover/movie?api_key=' + key;
-
-export default getMovies(url);
